9267019bcb9914e5eff9ca46f974ad011238a5cb
mill1514 <rylemil@gmail.com>
2020-10-04 20:42:35 -0400

    whattup pimp something works

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python
 
-import requests
+from selenium import webdriver
 
 def main():
-	targetUrl = "https://www.walmart.com/ip/ICE-MOUNTAIN-Brand-100-Natural-Spring-Water-16-9-ounce-bottles-Pack-of-24/21943694"
-	response = requests.get(targetUrl)
-	print(response.status_code)
-	print(response.text)
+	browser = webdriver.Chrome()
+	browser.get("https://www.yahoo.com")
 
 main()
