8d61aed5cddcf99d1865330ac1d67bb20bfa9b20
mill1514 <rylemil@gmail.com>
2020-10-04 20:04:54 -0400

    [main] 	another update playa

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python
 
+import requests
+
 def main():
-	print "Hello, world!"
+	targetUrl = "https://www.walmart.com/ip/ICE-MOUNTAIN-Brand-100-Natural-Spring-Water-16-9-ounce-bottles-Pack-of-24/21943694"
+	response = requests.get(targetUrl)
+	print(response.status_code)
+	print(response.text)
 
 main()
