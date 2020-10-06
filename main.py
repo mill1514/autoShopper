c5a5f5c9c81de15d21e808d7902a2069bbe66752
mill1514 <rylemil@gmail.com>
2020-10-05 21:01:39 -0400

    baby's first click()

--- main.py
+++ main.py
@@ -1,9 +1,22 @@
 #!/usr/bin/python
 
+import time
 from selenium import webdriver
+from selenium.webdriver.common.action_chains import ActionChains 
+
+browser = webdriver.Chrome()
+action = ActionChains(browser)
 
 def main():
-	browser = webdriver.Chrome()
-	browser.get("https://www.yahoo.com")
+	# Head on over to Walmart
+	browser.get("https://www.walmart.com/ip/ICE-MOUNTAIN-Brand-100-Natural-Spring-Water-16-9-ounce-bottles-Pack-of-24/21943694")
+	time.sleep(1)
+
+	# Find the "add to cart" button
+	targetClass = "prod-product-cta-add-to-cart"
+	button = browser.find_element_by_class_name(targetClass)
+	action.click(on_element=button)
+	action.perform()
+	time.sleep(5)
 
 main()
