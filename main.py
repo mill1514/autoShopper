6b7457902a8a4aa60e028a9fcc96bd7f81679d9e
mill1514 <rylemil@gmail.com>
2020-10-06 21:11:15 -0400

    Multiple clicks. yeah boi

--- main.py
+++ main.py
@@ -5,18 +5,26 @@ from selenium import webdriver
 from selenium.webdriver.common.action_chains import ActionChains 
 
 browser = webdriver.Chrome()
-action = ActionChains(browser)
 
 def main():
 	# Head on over to Walmart
 	browser.get("https://www.walmart.com/ip/ICE-MOUNTAIN-Brand-100-Natural-Spring-Water-16-9-ounce-bottles-Pack-of-24/21943694")
-	time.sleep(1)
 
-	# Find the "add to cart" button
-	targetClass = "prod-product-cta-add-to-cart"
-	button = browser.find_element_by_class_name(targetClass)
-	action.click(on_element=button)
-	action.perform()
+	# Click the "add to cart" button
+	addToCartButtonClass = "prod-product-cta-add-to-cart"
+	addToCartButton = browser.find_element_by_class_name(addToCartButtonClass)
+
+	addToCartAction = ActionChains(browser)
+	addToCartAction.click(on_element=addToCartButton)
+	addToCartAction.perform()
+
 	time.sleep(5)
+
+	checkoutButtonClass = "checkoutBtn"
+	checkoutButton = browser.find_element_by_class_name(checkoutButtonClass)
+
+	checkoutAction = ActionChains(browser)
+	checkoutAction.click(on_element=checkoutButton)
+	checkoutAction.perform()
 
 main()
